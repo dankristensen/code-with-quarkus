c33c02d5e44b012664fcb98079b2f7813a95fff2
Dan Kristensen <dk@cofman.com>
2025-01-13 19:27:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# code-with-quarkus
+
+This project uses Quarkus, the Supersonic Subatomic Java Framework.
+
+If you want to learn more about Quarkus, please visit its website: <https://quarkus.io/>.
+
+# Verification steps for slow running tests on Windows 11 (3.17.6)
+1. Do clean
+2. Do build (Will ensure all dependencies are downloaded)
+3. Do build again and record the time it takes.
+4. Do build again and record the time it takes.
+5. Run the test GreetingResourceTest from your IDE (IntelliJ), and record the "Build and Run execution times"
+
+# Verification steps for slow running tests on Windows 11 (999-SNAPSHOT)
+1. Do clean
+2. Do build (Will ensure all dependencies are downloaded)
+3. Do build again and record the time it takes.
+4. Do build again and record the time it takes.
+5. Run the test GreetingResourceTest from your IDE (IntelliJ), and record the "Build and Run execution times"
+
---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    java
+    id("io.quarkus")
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven {
+        name = "quarkus-snapshots-repository"
+        url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+    }
+}
+
+val quarkusPlatformGroupId: String by project
+val quarkusPlatformArtifactId: String by project
+val quarkusPlatformVersion: String by project
+
+dependencies {
+    implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
+    implementation("io.quarkus:quarkus-rest")
+    implementation("io.quarkiverse.wiremock:quarkus-wiremock:1.4.0")
+    implementation("io.quarkus:quarkus-arc")
+    testImplementation("io.quarkus:quarkus-junit5")
+    testImplementation("io.rest-assured:rest-assured")
+    testImplementation("org.mockito:mockito-core:5.14.2")
+    testImplementation("org.hamcrest:hamcrest:3.0")
+
+}
+
+group = "org.acme"
+version = "1.0.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+tasks.withType<Test> {
+    systemProperty("java.util.logging.manager", "org.jboss.logmanager.LogManager")
+}
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.compilerArgs.add("-parameters")
+}
